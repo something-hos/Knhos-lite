354be96c664e981c6405ad4f75048c6fad10aed3
something-hos <fcbpavan@gmail.com>
2026-08-26 13:16:49 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@
  * All patient data lives in IndexedDB, never in this cache.
  */
 
-const CACHE_VERSION = 'knhos-lite-shell-v4';
+const CACHE_VERSION = 'knhos-lite-shell-v5';
 
 const SHELL_FILES = [
   './',
